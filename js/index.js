050dfe78c2c39ec57ece1683164b6f0067709d84
booldook <booldook@gmail.com>
2020-03-12 13:12:05 +0900

    지도 확대 축소 완료

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 // 4c302863cccc615af46f096f1ea543b9
 
 var api = 'https://8oi9s0nnth.apigw.ntruss.com/corona19-masks/v1/storesByGeo/json';
-var lat, lng, m = 1000, map, positions;
+var lat, lng, m = 1000, map, positions, zoom;
 var mapContainer = document.getElementById('map');
 var img = {
 	plenty: './img/marker-green.svg', 
@@ -10,29 +10,46 @@ var img = {
 	few: './img/marker-red.svg',
 	empty: './img/marker-grey.svg',
 };
+var meter = [0, 500, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000];
 
 function init() {
+	// 브라우저(디바이스)의 위치정보 가져오기
 	navigator.geolocation.getCurrentPosition(function(pos) {
 		if(pos) {
+			// 위치정보 가져왔을 때
 			lat = pos.coords.latitude;
 			lng = pos.coords.longitude;
 		}
 		else {
+			// 위치정보 못가져 왔을 때
 			lat = 37.401127;
 			lng = 126.920475;
 		}
-		$.get(api, {lat: lat, lng: lng, m: m}, getStores);
+
+		// 지도 생성
 		var mapOption = { 
 			center: new kakao.maps.LatLng(lat, lng),
 			level: 3 
 		};
 		map = new kakao.maps.Map(mapContainer, mapOption);
+
+		// 지도 이동 이벤트
 		kakao.maps.event.addListener(map, 'dragend', function() {
 			var latlng = map.getCenter(); 
 			lat = latlng.getLat();
 			lng = latlng.getLng();
-			$.get(api, {lat: lat, lng: lng, m: m}, getStores);
+			zoom = map.getLevel();
+			$.get(api, {lat: lat, lng: lng, m: meter[zoom]}, getStores);
 		});
+
+		// 지도 확대 축소 이벤트
+		kakao.maps.event.addListener(map, 'zoom_changed', function() {
+			zoom = map.getLevel();
+			$.get(api, {lat: lat, lng: lng, m: meter[zoom]}, getStores);
+		});
+
+		//약국정보 요청
+		$.get(api, {lat: lat, lng: lng, m: meter[3]}, getStores);
 	});
 }
 
@@ -69,57 +86,3 @@ function setMap() {
 }
 
 init();
-
-
-
-
-
-/*
-var mapContainer = document.getElementById('map'), // 지도를 표시할 div  
-		mapOption = { 
-				center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
-				level: 3 // 지도의 확대 레벨
-		};
-
-var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
- 
-// 마커를 표시할 위치와 title 객체 배열입니다 
-var positions = [
-		{
-				title: '카카오', 
-				latlng: new kakao.maps.LatLng(33.450705, 126.570677)
-		},
-		{
-				title: '생태연못', 
-				latlng: new kakao.maps.LatLng(33.450936, 126.569477)
-		},
-		{
-				title: '텃밭', 
-				latlng: new kakao.maps.LatLng(33.450879, 126.569940)
-		},
-		{
-				title: '근린공원',
-				latlng: new kakao.maps.LatLng(33.451393, 126.570738)
-		}
-];
-
-// 마커 이미지의 이미지 주소입니다
-var imageSrc = "http://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png"; 
-		
-for (var i = 0; i < positions.length; i ++) {
-		
-		// 마커 이미지의 이미지 크기 입니다
-		var imageSize = new kakao.maps.Size(24, 35); 
-		
-		// 마커 이미지를 생성합니다    
-		var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize); 
-		
-		// 마커를 생성합니다
-		var marker = new kakao.maps.Marker({
-				map: map, // 마커를 표시할 지도
-				position: positions[i].latlng, // 마커를 표시할 위치
-				title : positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
-				image : markerImage // 마커 이미지 
-		});
-}
-*/
